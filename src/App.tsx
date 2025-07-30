2a527fd6dfc3d39adf57d806b0bd01ff06640e87
Oluwadara Oloye <daraoloye99@gmail.com>
2025-07-30 21:24:32 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-[57px] font-medium tracking-[-0.25px] leading-[64px] text-app-purple dark:text-app-blue font-roboto">
+        Frequently asked questions
+      </h1>
+    </div>
+  )
+}
+
+export default App
